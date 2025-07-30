eb78626d47e3a4034b7b2920eceff0f421fa38a1
Ammar Mian <ammar.mian@univ-smb.fr>
2025-07-30 11:10:42 +0200

    ledoitwolf simulation in context

--- 1-context/lwf_underregime.py
+++ 1-context/lwf_underregime.py
@@ -1,4 +1,4 @@
-# Showing behavior of SCM on simple data with case dim  > N_obs
+# Showing behavior of LWF  on simple data with case dim  > N_obs
 
 import argparse
 
@@ -18,34 +18,35 @@ plt.rcParams["text.latex.preamble"] = r"\usepackage{amsmath}"
 
 
 def compute_estimation(params):
-    d, N, trial_no = params
+    d, N, trial_no, alpha = params
     rng = np.random.default_rng(
         d + N + trial_no
     )  # To be sure the generation of data is different
     mean = np.ones((d, N))
     data = mean + rng.standard_normal((d, N))
-
-    estimate_mean = np.mean(data, axis=1)
-    estimate_cov = np.cov(data)
+    estimate_cov_scm = np.cov(data)
+    estimate_cov_lwf = (1 - alpha) * estimate_cov_scm + alpha * np.eye(d)
     return [
         d,
-        np.linalg.norm(estimate_mean - mean[:, 0]),
-        np.linalg.norm(estimate_cov - np.eye(d), ord="fro"),
-        np.linalg.cond(estimate_cov),
+        np.linalg.cond(estimate_cov_scm),
+        np.linalg.cond(estimate_cov_lwf),
     ]
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        "MC simulation of estimating mean and covariance with increasing number of variables."
+        "MC simulation of estimating covariance with increasing number of variables (lwf)"
     )
     parser.add_argument("--N", type=int, default=30, help="Number of observations.")
     parser.add_argument("--n_trials", type=int, default=50, help="Number of MC-trials.")
     parser.add_argument(
         "--output_dir",
         type=str,
-        default="outputs/error_estimation_scm_underregime",
+        default="outputs/error_estimation_lwf_underregime",
         help="Directory for LaTeX exports.",
+    )
+    parser.add_argument(
+        "--alpha", type=float, default=0.1, help="Coefficient of regularization."
     )
     args = parser.parse_args()
 
@@ -57,7 +58,7 @@ if __name__ == "__main__":
     # Montecarlo with progressbar.
     # Inspired from: https://github.com/Textualize/rich/discussions/884#discussioncomment-269200
     print("Launching simulation")
-    param_grid = product(d_vec, [N], list(range(n_trials)))
+    param_grid = product(d_vec, [N], list(range(n_trials)), [args.alpha])
     results = []
     with Progress() as progress:
         task_id = progress.add_task("[cyan]Working...", total=len(d_vec) * n_trials)
@@ -69,47 +70,26 @@ if __name__ == "__main__":
         results = np.stack(results)
 
     # Averaging monte-carlo
-    error_mean_mean = np.stack([results[results[:, 0] == d, 1].mean() for d in d_vec])
-    error_mean_std = np.stack([results[results[:, 0] == d, 1].std() for d in d_vec])
+    cond_scm_mean = np.stack([results[results[:, 0] == d, 1].mean() for d in d_vec])
+    cond_scm_std = np.stack([results[results[:, 0] == d, 1].std() for d in d_vec])
 
-    error_cov_mean = np.stack([results[results[:, 0] == d, 2].mean() for d in d_vec])
-    error_cov_std = np.stack([results[results[:, 0] == d, 2].std() for d in d_vec])
-
-    cond_cov_mean = np.stack([results[results[:, 0] == d, 3].mean() for d in d_vec])
-    cond_cov_std = np.stack([results[results[:, 0] == d, 3].std() for d in d_vec])
+    cond_lwf_mean = np.stack([results[results[:, 0] == d, 2].mean() for d in d_vec])
+    cond_lwf_std = np.stack([results[results[:, 0] == d, 2].std() for d in d_vec])
     print("Done.")
 
     # Plotting
     fig = plt.figure()
-    plt.scatter(d_vec, error_mean_mean, marker="o", facecolors="none", edgecolors="k")
-    plt.errorbar(d_vec, error_mean_mean, yerr=error_mean_std, linestyle="", capsize=5)
-    plt.xlabel(r"$d$")
-    plt.ylabel(
-        r"$\|\hat{\boldsymbol{\mu}}_\mathcal{X} - \boldsymbol{\mu}_\mathcal{X}\|_2$"
+    plt.scatter(
+        d_vec, cond_scm_mean, marker="o", facecolors="none", edgecolors="k", label="scm"
     )
-    plt.xscale("log")
-    plt.yscale("log")
-    plt.title(f"Error of mean estimation with {n_trials} Monte-carlo trials")
-    save_path = os.path.join(args.output_dir, "mean.tex")
-    save(save_path)
-    print(f"Saved mean error in {save_path}")
-
-    fig = plt.figure()
-    plt.scatter(d_vec, error_cov_mean, marker="o", facecolors="none", edgecolors="k")
-    plt.errorbar(d_vec, error_cov_mean, yerr=error_cov_std, linestyle="", capsize=5)
-    plt.xlabel("$d$")
-    plt.xscale("log")
-    plt.yscale("log")
-    plt.ylabel(
-        r"$\|\hat{\boldsymbol{\Sigma}}_\mathcal{X} - \boldsymbol{\Sigma}_\mathcal{X}\|_2$"
+    plt.scatter(
+        d_vec,
+        cond_lwf_mean,
+        marker="s",
+        facecolors="none",
+        edgecolors="k",
+        label="lwf",
     )
-    plt.title(f"Error of mean estimation with {n_trials} Monte-carlo trials")
-    save_path = os.path.join(args.output_dir, "cov.tex")
-    save(save_path)
-    print(f"Saved cov error in {save_path}")
-
-    fig = plt.figure()
-    plt.scatter(d_vec, cond_cov_mean, marker="o", facecolors="none", edgecolors="k")
     # plt.errorbar(d_vec, cond_cov_mean, yerr=cond_cov_std, linestyle="", capsize=5)
     plt.axvline(x=N, color="k", label=r"$d=N$", linestyle="--")
     plt.xlabel("$d$")
